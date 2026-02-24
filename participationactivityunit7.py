566efa7092c57049faba4d0369f7cf46b6b9241d
Ryma Djoudad <zenyod85@gmail.com>
2026-02-24 10:19:27 -0500

    init commit

---
+++ participationactivityunit7.py
@@ -0,0 +1,8 @@
+prompt = "If you share your name, we can personalize the message you see."
+prompt += "\nWhat is your first name? "
+
+message = ""
+
+while message != "quit":
+    message = input(prompt)
+    print(message)
